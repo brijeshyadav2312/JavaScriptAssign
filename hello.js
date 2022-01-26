45b67abb86975adee4c33c9fbb7792010896b68c
Brijesh Yadav <88343913+brijeshyadav2312@users.noreply.github.com>
2022-01-26 17:31:49 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,14 @@
+var a = 12
+
+if (a == 10 )
+{
+	console.log("Given number is equal to 10")
+}
+else if(a>10)
+{
+	console.log("Given number is greater than 10")
+}
+else
+{
+	console.log("Given number is less than 10")
+}

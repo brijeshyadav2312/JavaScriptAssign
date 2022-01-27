61e67c4c6bc6a763ad72379f71ee39d8f7b36c59
Brijesh Yadav <88343913+brijeshyadav2312@users.noreply.github.com>
2022-01-27 19:20:18 +0530

    Create assign2.js

---
+++ 26 Jan/assign2/assign2.js
@@ -0,0 +1,11 @@
+var n=prompt("Enter character");
+if(n=='a' || n=='e' || n=='i' || n=='o' || n=='u')
+{
+   console.log("Given Character is Vowel ");
+}
+else if(n=='A' || n=='E' || n=='I' || n=='O' || n=='U'){
+   console.log("Given Character is Vowel ");
+}
+else{
+   console.log("Given Character is Consonant ");
+}

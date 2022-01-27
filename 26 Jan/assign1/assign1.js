0fea6784bd4291def1b0bbac13dc408abf961b03
Brijesh Yadav <88343913+brijeshyadav2312@users.noreply.github.com>
2022-01-27 19:18:30 +0530

    Create assign1.js

---
+++ 26 Jan/assign1/assign1.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter First Number")
+var b=prompt("Enter second Number")
+var c=prompt("Enter third Number")
+if(a<=b && a<=c){
+   alert(a)
+}
+else if(b<=a && b<=c){
+   alert(b)
+}
+else{
+   alert(c)
+}

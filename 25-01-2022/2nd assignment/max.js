56e73ce995fcc09579ab3143b6eda66caeda2888
Brijesh Yadav <88343913+brijeshyadav2312@users.noreply.github.com>
2022-02-06 15:50:08 +0530

    Add files via upload

---
+++ 25-01-2022/2nd assignment/max.js
@@ -0,0 +1,16 @@
+var a=10
+var b=11
+var c=2
+
+if(a>=b && a>=c)
+{
+cosole.log("maximum number is ",a)
+}
+else if(b>=a && b>=c)
+{
+console.log("maximum number is ",b)
+}
+else
+{
+console.log("maximim number is ",c)
+}
